cd244b1b948c934cf1e44298573d89e54c003676
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-05-15 06:45:53 +0200

    Add PersonalInfo form with form submission hook and CSS style.

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -1,23 +1,8 @@
-import { Playground } from './components/Playground/Playground';
-import Home from './pages/Home';
+import PersonalInfoForm from './components/PersonalInFo/PersonalInfoForm.jsx';
 
-const IS_PLAYGROUND_ENABLED = import.meta.env.VITE_PLAYGROUND_ENABLED;
-
-/**
- * Routes for the application.
- */
 export const routes = [
   {
     path: '/',
-    element: <Home />,
+    element: <PersonalInfoForm />,
   },
-  // Define your routes here
-  ...(IS_PLAYGROUND_ENABLED === 'true'
-    ? [
-        {
-          path: '/playground/component/:componentName',
-          element: <Playground />,
-        },
-      ]
-    : []),
 ];
