fb8f48eb2fe2da13f626e8eea3285626c88ccde8
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-05-19 10:08:56 +0200

    Update changes for Personal Inforation Form

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -1,8 +1,25 @@
-import PersonalInfoForm from './components/PersonalInFo/PersonalInfoForm.jsx';
+import { Playground } from './components/Playground/Playground';
+import Home from './pages/Home';
+import PersonalInfoForm from './components/PersonalInfo/PersonalInfoForm.jsx';
 
+const IS_PLAYGROUND_ENABLED = import.meta.env.VITE_PLAYGROUND_ENABLED;
+
+/**
+ * Routes for the application.
+ */
 export const routes = [
+  // Define your routes here
+
   {
     path: '/',
     element: <PersonalInfoForm />,
   },
+  ...(IS_PLAYGROUND_ENABLED === 'true'
+    ? [
+        {
+          path: '/playground/component/:componentName',
+          element: <Playground />,
+        },
+      ]
+    : []),
 ];
