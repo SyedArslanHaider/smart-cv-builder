ef55276bf13966519550212ea30877055f988e26
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-05-22 14:11:55 +0200

    Updated changes for header-logo

--- web/src/components/Header/Header.jsx
+++ web/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from '../Header/Header.module.css';
-import Logo from '/home/l0086/Desktop/smart-cv-builder/web/src/assets/Logo.png';
+import styles from './Header.module.css';
+import Logo from '../../assets/Logo.png';
 
 const Header = () => {
   return (
