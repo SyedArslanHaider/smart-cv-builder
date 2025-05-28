9e1fe4a02edd324774598defff5cd8088c044601
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-05-28 08:14:40 +0200

    Add ProfileVsJob Component with Css Style

---
+++ web/src/components/ProfileVsJob/ProfileVsJob.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styles from './ProfileVsJob.module.css';
+
+const ProfileVsJob = () => {
+  const [jobcriteria, setJobCriteria] = useState('');
+  const [error, setError] = useState('');
+
+  const validateJobCriteria = () => {
+    if (!jobcriteria.trim()) {
+      setError('Please provide the profile vs job criteria.');
+    } else if (jobcriteria.length < 200) {
+      setError('The Criteria must be at least 200 characters long.');
+    } else {
+      setError('');
+    }
+  };
+
+  const handleChange = (e) => {
+    setJobCriteria(e.target.value);
+    validateJobCriteria();
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}> PROFILE vs. JOB CRITERIA</h1>
+
+      <label htmlFor="profile-vs-job" className={styles.label}>
+        What Helps You Stand Out?
+      </label>
+      <textarea
+        id="profile-vs-job"
+        className={`${styles.textarea} ${error ? styles.errortextarea : ''}`}
+        placeholder="Excellent teamwork, problem-solving, and adaptability. Proficient in React, Express, PostgreSQL, and Agile methodologies. Fluent in English and intermediate Spanish. Meet job criteria in full-stack development and collaborative project execution."
+        value={jobcriteria}
+        onChange={handleChange}
+        required
+      />
+
+      {error && <p className={styles.errortext}>{error}</p>}
+    </div>
+  );
+};
+
+export default ProfileVsJob;
