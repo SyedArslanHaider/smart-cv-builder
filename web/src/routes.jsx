05b46a6f9b6209ca4ad617fc1fa22b9c4e218926
Afere precious <170966313+AferePreciousOnome@users.noreply.github.com>
2025-05-29 18:36:39 +0200

    Cleaned up my routes

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -1,16 +1,5 @@
 import { Playground } from './components/Playground/Playground';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Home from './pages/Home';
-=======
-<<<<<<< HEAD
-=======
-import Home from './pages/Home';
->>>>>>> parent of 2a64ff9 (Revert "Add Education component with css styling.")
->>>>>>> parent of f556b7b (Took out the education component to avoid conflicts.)
-=======
 
->>>>>>> main
 import PersonalInfoForm from './components/PersonalInfo/PersonalInfoForm.jsx';
 
 const IS_PLAYGROUND_ENABLED = import.meta.env.VITE_PLAYGROUND_ENABLED;
