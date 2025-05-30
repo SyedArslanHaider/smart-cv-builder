77f9ff5047285d22834897be9c5bd9aa75b421bd
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-05-30 13:13:03 +0200

    Add aNext and Previous Buttons

---
+++ web/src/components/NextButton/NextButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './NextButton.module.css';
+
+const NextButton = () => {
+  return <button className={styles.nextbutton}>Next</button>;
+};
+
+export default NextButton;
---
+++ web/src/components/PreviousButton/PreviousButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './PreviousButton.module.css';
+
+const PreviousButton = () => {
+  return <button className={styles.previousbutton}>Previous</button>;
+};
+
+export default PreviousButton;
