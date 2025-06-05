0585dac982cf5e233af9d3f056ba919a9ef9fb4e
Priscilla Mac-Gatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-06-05 09:48:01 +0200

    Next, Previous  and Submit Button  Components

---
+++ web/src/components/SubmitButton/SubmitButton.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './SubmitButton.module.css';
+
+const SubmitButton = () => {
+  return <button className={styles.submitbutton}>Submit</button>;
+};
+
+export default SubmitButton;
