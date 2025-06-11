70bbe63c1e72d40061b13bd88d3d60faf318ff33
Priscilla MacGatus <176624280+Priscilla-MacGatus@users.noreply.github.com>
2025-06-11 21:27:49 +0200

    Update routes.jsx
    Removed the Dummy for page routes and file path

--- web/src/routes.jsx
+++ web/src/routes.jsx
@@ -1,7 +1,6 @@
 import { Playground } from './components/Playground/Playground';
 import Home from './pages/Home';
 import PreviewPage from './pages/PreviewPage/PreviewPage';
-import DummyFormPage from './components/DummyFormPage/DummyFormPage';
 import { Navigate } from 'react-router-dom';
 
 const IS_PLAYGROUND_ENABLED = import.meta.env.VITE_PLAYGROUND_ENABLED;
@@ -10,10 +9,6 @@ export const routes = [
   {
     path: '/',
     element: <Navigate to="/form" replace />, // ✅ root route redirect
-  },
-  {
-    path: '/form',
-    element: <DummyFormPage />,
   },
   {
     path: '/preview',
