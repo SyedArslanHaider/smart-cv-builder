fab3c7c5575f4970a2ba18f1739684c12e28eec1
rachealgit <racheal.ehiwe29@gmail.com>
2025-06-12 01:07:09 +0200

    Add reverted changes

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router';
 import './index.css';
 import { routes } from './routes';
 
