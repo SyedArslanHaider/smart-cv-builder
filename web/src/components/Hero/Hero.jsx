3c4a158fa2ae1a880eb9451d04fe92a0aa7ee080
Racheal Ehiwe <128506248+Rachealgit@users.noreply.github.com>
2025-06-12 09:35:22 +0200

    Update Hero.jsx

--- web/src/components/Hero/Hero.jsx
+++ web/src/components/Hero/Hero.jsx
@@ -1,37 +1,36 @@
-    
-    import React from 'react';
-    import styles from './Hero.module.css';
-    import Rotating3DCV from '../Rotating3DCV/Rotating3DCV';
-    import { Link } from 'react-router-dom';
+import React from 'react';
+import styles from './Hero.module.css';
+import Rotating3DCV from '../Rotating3DCV/Rotating3DCV';
+import { Link } from 'react-router-dom';
 
-    const Hero = () => {
-    return (
-        <section className={styles.hero}>
-        <div className={styles.container}>
-            <div className={styles.left}>
-            <Rotating3DCV />
-            </div>
+const Hero = () => {
+return (
+<section className={styles.hero}>
+<div className={styles.container}>
+    <div className={styles.left}>
+    <Rotating3DCV />
+    </div>
 
-            <div className={styles.right}>
-            <h1 className={styles.title}>
-                Your <span className={styles.highlight}>Journey</span> deserves a
-                resume that Reflects it.
-            </h1>
-            <p className={styles.subtitle}>
-                Land your first tech job with a CV that turns your journey into your
-                superpower.
-            </p>
-            <div className={styles.buttonRow}>
-                <Link to="/form">
-                <button className={styles.primaryBtn}>Create My Resume</button>
-                </Link>
+    <div className={styles.right}>
+    <h1 className={styles.title}>
+        Your <span className={styles.highlight}>Journey</span> deserves a
+        resume that Reflects it.
+    </h1>
+    <p className={styles.subtitle}>
+        Land your first tech job with a CV that turns your journey into your
+        superpower.
+    </p>
+    <div className={styles.buttonRow}>
+        <Link to="/form">
+        <button className={styles.primaryBtn}>Create My Resume</button>
+        </Link>
 
-                <button className={styles.secondaryBtn}>View GitHub Doc</button>
-            </div>
-            </div>
-        </div>
-        </section>
-    );
-    };
+        <button className={styles.secondaryBtn}>View GitHub Doc</button>
+    </div>
+    </div>
+</div>
+</section>
+);
+};
 
-    export default Hero;
+export default Hero;
