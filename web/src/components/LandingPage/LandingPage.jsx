cd4b7854c138c6e4b905e5e89faf68a6017b0207
Racheal Ehiwe <128506248+Rachealgit@users.noreply.github.com>
2025-06-12 09:31:31 +0200

    Update LandingPage.jsx

--- web/src/components/LandingPage/LandingPage.jsx
+++ web/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,12 @@
     import React from 'react';
-    import Header from '../../components/Header/Header.jsx';
-    import Hero from '../../components/Hero/Hero.jsx';
+    import Header from '../Header/Header.jsx';
+    import Hero from '../Hero/Hero.jsx';
     const LandingPage = () => {
     return (
-        <div>
+        <>
         <Header />
         <Hero/>
-        </div>
+        </>
     );
     };
     export default LandingPage;
